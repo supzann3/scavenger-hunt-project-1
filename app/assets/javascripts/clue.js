3ecb40179e1c8f311f17b3c6ad106f69a6be8037
supzann3 <susan.hehe01@gmail.com>
2015-09-22 16:15:22 -0400

    add video-1

--- app/assets/javascripts/clue.js
+++ app/assets/javascripts/clue.js
@@ -33,31 +33,31 @@ function getCoords() {
     }
 }
 
-function sendPosition(position) {
-
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    $('#test').append("<div>Latitude:  " + latitude + "</div");
-    $('#test').append("<div>Longitude:  " + longitude + "</div");
-    $.ajax({
-      type: "POST",
-      url: "/lists/:list_id/clues/:id",
-      data: {latitude: latitude, longitude: longitude, answer: $("#answer").val(), clue_id: $("#clue_id").val(), current_list_id: $("#current_list_id").val()}
-  }).done(function(response){
-    // debugger
-    $('#clue-form').html(response);
-  });
-}
-
-function sendCluePosition(position) {
-
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    $.ajax({
-      type: "POST",
-      url: "/lists/" + $("#current_list_id").val() + "/clues",
-      data: {latitude: latitude, longitude: longitude, text: $("#text").val(), answer: $("#answer").val(), current_list_id: $("#current_list_id").val()}
-  }).done(function(response){
-    $('#new-clue-form').html(response);
-  });
-}
+// function sendPosition(position) {
+//
+//     var latitude = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//     $('#test').append("<div>Latitude:  " + latitude + "</div");
+//     $('#test').append("<div>Longitude:  " + longitude + "</div");
+//     $.ajax({
+//       type: "POST",
+//       url: "/lists/:list_id/clues/:id",
+//       data: {latitude: latitude, longitude: longitude, answer: $("#answer").val(), clue_id: $("#clue_id").val(), current_list_id: $("#current_list_id").val()}
+//   }).done(function(response){
+//     // debugger
+//     $('#clue-form').html(response);
+//   });
+// }
+//
+// function sendCluePosition(position) {
+//
+//     var latitude = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//     $.ajax({
+//       type: "POST",
+//       url: "/lists/" + $("#current_list_id").val() + "/clues",
+//       data: {latitude: latitude, longitude: longitude, text: $("#text").val(), answer: $("#answer").val(), current_list_id: $("#current_list_id").val()}
+//   }).done(function(response){
+//     $('#new-clue-form').html(response);
+//   });
+// }
