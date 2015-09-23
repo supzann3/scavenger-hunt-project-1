80086116e11e442032ae9ee9c0968faa7c81754a
supzann3 <susan.hehe01@gmail.com>
2015-09-23 16:20:57 -0400

    add scavenger-hunt title

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-List.create(name:"Test List", location:"FIS")
-Clue.create(list_id: 1, text: "Who was the first president of the US?", answer:"George Washington")
-Clue.create(list_id: 1, text: "What is 19 - 5", answer:"14")
-Clue.create(list_id: 1, text: "What is is the biggest state in the United States by size?", answer:"Alaska")
+List.create(name:"Chinese Cusine", location:"Chinatown")
+Clue.create(list_id: 2,latitude: 40.7159716,longitude:-73.9971711, text: "With more than 1,300 reviews this restaurant has the best Dim Sum", answer:"Jing Fong Restaurant")
+Clue.create(list_id: 2, latitude: 40.714655,longitude:-73.9977528, text: "Shanghai best soup dumplings have been brought by (rhythmes with toe)", answer:"Joe’s Shanghai")
+Clue.create(list_id: 2, latitude:40.7162159,longitude:-73.9983124, text: "Best Roast Pork in Mott st", answer:"Big Wong King")
+Clue.create(list_id: 2, latitude:40.7165432,longitude:-73.999068, text: "Sweet Egg Tarts' Bakery", answer:"Lung Moon Bakery")
+Clue.create(list_id: 2, latitude:40.7165432,longitude:-73.999068, text: "First Bubble Tea in Chinatown", answer:"Ten Ren’s Tea Time")
